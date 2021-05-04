3a218d232d9db9214e4dda8c22b6630a9ce93e65
Nikola Kharkevich <nikolay_kharkevych@MacBook-Pro-Nikolay.local>
2021-05-04 20:33:34 +0300

    Initial commit

---
+++ Netology_IB_Instruments'/ProfileView.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments'
+//
+//  Created by Nikola Kharkevich on 04.05.2021.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var birthday: UILabel!
+    @IBOutlet weak var placeOfBirth: UILabel!
+    @IBOutlet weak var userPhoto: UIImageView!
+    @IBOutlet weak var userInfo: UITextView!
+    
+}
---
+++ Netology_IB_Instruments'/ProfileViewController.swift
@@ -0,0 +1,12 @@
+//
+//  ProfileViewController.swift
+//  Netology_IB_Instruments'
+//
+//  Created by Nikola Kharkevich on 04.05.2021.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+    
+}
